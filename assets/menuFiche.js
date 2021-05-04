d4d801c0d6b37557b016271d713b92e42e589074
Antoine Mrn <58473024+AntoineMra@users.noreply.github.com>
2021-05-04 15:53:42 +0200

    Update menuFiche.js

--- assets/menuFiche.js
+++ assets/menuFiche.js
@@ -1,3 +1,9 @@
 const creafiche = document.getElementById('creaFiche')
 const modifFiche = document.getElementById('modifFiche')
 
+<<<<<<< Updated upstream
+=======
+if (condition) {
+    
+}
+>>>>>>> Stashed changes
